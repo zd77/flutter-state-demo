81586f8aa2a125fece9ccec4ec155d86b0ef0e49
Dereck Z <zavaladereck77@gmail.com>
2023-03-30 19:55:48 -0600

    Original example

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'State Demo App',
       initialRoute: 'todo',
